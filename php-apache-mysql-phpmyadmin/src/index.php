e0d6f58af7a999dcefc70ab8d78af8ceb0f5ab55
Yoriiis <joris.daniel@gmail.com>
2023-03-12 22:09:38 +0100

    Update php apache stack with dot env

--- php-apache-mysql-phpmyadmin/src/index.php
+++ php-apache-mysql-phpmyadmin/src/index.php
@@ -1,21 +1,29 @@
 <?php
-    echo "Hello, world! The database is connected.<br>";
+    require __DIR__.'/vendor/autoload.php';
 
-    $dsn = 'mysql:host=mysql;dbname=app';
-    $user = 'root';
-    $password = 'root';
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
 
-    $pdo = new PDO($dsn, $user, $password, [
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES => false,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    ]);
+    $dsn = 'mysql:host=mysql;dbname='.$_ENV['MYSQL_DATABASE'];
+    $user = $_ENV['MYSQL_USER'];
+    $password = $_ENV['MYSQL_PASSWORD'];
 
-    $stmt = $pdo->query('SELECT * FROM articles');
+    try {
+        $pdo = new PDO($dsn, $user, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ]);
+        echo "Hello, world! The database is connected.<br><br>";
 
-    foreach ($stmt as $row) {
-        echo $row['title'] . '<br>';
-        echo $row['description'] . '<br>';
-        echo $row['date'] . '<br>';
+        $stmt = $pdo->query('SELECT * FROM articles');
+        foreach ($stmt as $row) {
+            echo $row['title'] . '<br>';
+            echo $row['description'] . '<br>';
+            echo $row['date'] . '<br>';
+        }
+
+    } catch (PDOException $e) {
+        echo 'Connection failed: ' . $e->getMessage();
     }
 ?>
